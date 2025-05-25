538c2aefa582b86c6fcf2a30e30e6606e82229c1
Srikaran26 <srikaranmodadugu@gmail.com>
2025-05-25 21:03:02 +0530

    UC 10: Add Validation to Name Field

--- src/main/java/com/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/addressbook/dto/AddressBookDTO.java
@@ -1,9 +1,17 @@
 package com.addressbook.dto;
 
-import lombok.Data;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class AddressBookDTO {
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Name must start with a capital letter and be at least 3 characters")
     private String name;
+
     private String address;
 }
